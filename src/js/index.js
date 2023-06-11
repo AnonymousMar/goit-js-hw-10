f331d801a46d47e26eb8f181046d2351c2b2d052
AnonymousMar <119819802+AnonymousMar@users.noreply.github.com>
2023-06-11 13:51:00 +0200

    .

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,21 @@
-import './css/styles.css';
+//import './css/styles.css';
 const refs = {
     searchForm: document.querySelector('.breed-select'),
     articlesContainer: document.querySelector('.cat-info'),
 };
-const options = {
-    headers: {
-        Autorization: 'live_8tpZg11OtSAVj1x4e7Xsgrtcx0d6GFYVaOUgrdFXq3fwB3BvcWUUBzK33KIQ8PCa'
-    }
-};
-const url = 'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng'
-fetch(url, options)
-    .then(response => response.json())
-    .then(console.log)
+refs.searchForm.addEventListener('click', onSearch);
+function onSearch(e) {
+    e.preventDefault();
+
+    const form = e.currentTarget.elements.query.value;
+
+    const options = {
+        headers: {
+            Autorization: 'live_8tpZg11OtSAVj1x4e7Xsgrtcx0d6GFYVaOUgrdFXq3fwB3BvcWUUBzK33KIQ8PCa'
+        }
+    };
+    const url = 'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng'
+    fetch(url, options)
+        .then(response => response.json())
+        .then(console.log)
+}
