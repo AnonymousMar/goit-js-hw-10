1c5d07e6192cfeef39bea26b484cf78e10d6958b
AnonymousMar <119819802+AnonymousMar@users.noreply.github.com>
2023-06-14 21:29:44 +0200

    ..

--- src/js/index.js
+++ src/js/index.js
@@ -1,48 +1,29 @@
-const url = `https://api.thecatapi.com/v1/breeds`;
-const api_key = "live_8tpZg11OtSAVj1x4e7Xsgrtcx0d6GFYVaOUgrdFXq3fwB3BvcWUUBzK33KIQ8PCa"
-let storedBreeds = []
-
-fetch(url, {
-    headers: {
-        'x-api-key': api_key
-    }
-})
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        data = data.filter(img => img.image?.url != null)
-        storedBreeds = data;
-
-        for (let i = 0; i < storedBreeds.length; i++) {
-            const breed = storedBreeds[i];
-            let option = document.createElement('option');
-
-            if (!breed.image) continue
-
-            option.value = i;
-            option.innerHTML = `${breed.name}`;
-            document.getElementById('breed_selector').appendChild(option);
-    } 
-        showBreedImage(0)
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
-
-function showBreedImage(index) {
-    document.getElementById("breed_image").src = storedBreeds[index].image.url;
-
-    document.getElementById("breed_json").textContent = storedBreeds[index].temperament
-
-
-    document.getElementById("wiki_link").href = storedBreeds[index].wikipedia_url
-    document.getElementById("wiki_link").innerHTML = storedBreeds[index].wikipedia_url
+async function fetchBreeds() {
+    const response = await fetch(" https://api.thecatapi.com/v1/breeds?api_key=live_8tpZg11OtSAVj1x4e7Xsgrtcx0d6GFYVaOUgrdFXq3fwB3BvcWUUBzK33KIQ8PCa")
+    const data = await response.json()
+    createBreedList(data.message)
 }
 
+fetchBreeds()
 
+function createBreedList(breedList) {
+    document.getElementById("breed").innerHTML = `
+  <select onchange="fetchCatByBreed(this.value) class="breed-select"">
+        ${Object.keys(breedList).map(function (breed) {
+            return `<option> ${breed} </option>`
+    }).join('')}
+      </select>
+  `
+}
 
+ async function loadByBreed(breed) {
+     if (breed != "Abysasiny") {
+         const response = await fetch(`https://api.thecatapi.com/v1/images/${breed}`)
+         const data = await response.json()
+         fetchCatByBreed(data.message)
+    }
+}
 
-
-
-
+function fetchCatByBreed(breedId) {
+    document.querySelector(".cat-info").innerHTML = `<div class="cat-info" style="background-image: url('${images[0]}')"></div>`
+}
