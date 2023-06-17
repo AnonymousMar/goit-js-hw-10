c1cb85091c6d877d2620a44722a4410bfeab3dc7
AnonymousMar <119819802+AnonymousMar@users.noreply.github.com>
2023-06-17 14:38:49 +0000

    Deploying to gh-pages from @ AnonymousMar/goit-js-hw-10@4dd9d29625ed8c3caac596715b8ba68a535a9a11 🚀

---
+++ index.3c429b82.js
@@ -0,0 +1,2 @@
+document.querySelector(".breed-select");
+//# sourceMappingURL=index.3c429b82.js.map
---
+++ index.9497ce29.js
@@ -0,0 +1,2 @@
+document.querySelector(".breed-select");
+//# sourceMappingURL=index.9497ce29.js.map
