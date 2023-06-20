74b131f05cbf2881014c32dcd0c735117ea0038c
AnonymousMar <119819802+AnonymousMar@users.noreply.github.com>
2023-06-20 16:28:09 +0200

    .

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,31 +1,41 @@
-export function fetchBreeds() {
-    const apiKey = 'live_8tpZg11OtSAVj1x4e7Xsgrtcx0d6GFYVaOUgrdFXq3fwB3BvcWUUBzK33KIQ8PCa'; 
-    const url = 'https://api.thecatapi.com/v1/breeds';
+   const API_KEY =
+      'live_8tpZg11OtSAVj1x4e7Xsgrtcx0d6GFYVaOUgrdFXq3fwB3BvcWUUBzK33KIQ8PCa';
 
-    return fetch(url, {
-        headers: {
-            'x-api-key': apiKey
-        }
-    })
-        .then(response => response.json())
-        .then(data => data)
-        .catch(error => {
+    export async function fetchBreeds() {
+        try {
+            const response = await fetch('https://api.thecatapi.com/v1/breeds', {
+                headers: {
+                    'x-api-key': API_KEY,
+                },
+            });
+            if (!response.ok) {
+                throw new Error('Failed to fetch breeds');
+            }
+            const data = await response.json();
+            return data.map(breed => ({ id: breed.id, name: breed.name }));
+        } catch (error) {
+            console.error('Error fetching breeds:', error);
             throw error;
-        });
-}
-
-export function fetchCatByBreed(breedId) {
-    const apiKey = 'live_8tpZg11OtSAVj1x4e7Xsgrtcx0d6GFYVaOUgrdFXq3fwB3BvcWUUBzK33KIQ8PCa'; 
-    const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-
-    return fetch(url, {
-        headers: {
-            'x-api-key': apiKey
         }
-    })
-        .then(response => response.json())
-        .then(data => data[0])
-        .catch(error => {
+    }
+
+    export async function fetchCatByBreed(breedId) {
+        try {
+            const response = await fetch(
+                `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`,
+                {
+                    headers: {
+                        'x-api-key': API_KEY,
+                    },
+                }
+            );
+            if (!response.ok) {
+                throw new Error('Failed to fetch cat information');
+            }
+            const data = await response.json();
+            return data[0];
+        } catch (error) {
+            console.error('Error fetching cat information:', error);
             throw error;
-        });
-}
+        }
+    }
